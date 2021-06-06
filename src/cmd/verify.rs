5dd2ac63a49d6f6894ec8ff4afeb95f0c546dfa6
Kelly Byrd <kbyrd@memcpy.com>
2021-06-06 07:29:29 -0700

    Output a seed phrase as part of `verify`.
    The verify command now takes the same --seed <bip39|mobile> option
    as the create command. This will add a mnemonic phrase to the output
    of `verify`. This phrase can be used to recreate the wallet.
    
    It is important for the user to specify the correct seed type. The
    same wallet will output different phrases when given different seed
    types. The difference is the checksum bits at then end.
    
    While it is possible to retrieve the phrase from a wallet  that was
    originally imported from the mobile app, it is not possible to take
    a cli-generated wallet (which has real 256-bits of entropy) and get
    a 12-word phrase that the mobile wallet will accept.
    
    TESTING:
    I have not actually tested importing from mobile to CLI then back
    to mobile.

--- src/cmd/verify.rs
+++ src/cmd/verify.rs
@@ -1,37 +1,71 @@
-use crate::{cmd::*, result::Result, wallet::Wallet};
+use crate::{cmd::*, keypair::Keypair, mnemonic::SeedType, result::Result, wallet::Wallet};
 use prettytable::{format, Table};
 use serde_json::json;
 
 /// Verify an encypted wallet
 #[derive(Debug, StructOpt)]
-pub struct Cmd {}
+pub struct Cmd {
+    #[structopt(long, possible_values = &["bip39", "mobile"], case_insensitive = true)]
+    /// Use a BIP39 or mobile app seed phrase to generate the wallet keys
+    seed: Option<SeedType>,
+}
 
 impl Cmd {
     pub async fn run(&self, opts: Opts) -> Result {
         let password = get_password(false)?;
         let wallet = load_wallet(opts.files)?;
-        let result = wallet.decrypt(password.as_bytes());
-        print_result(&wallet, result.is_ok(), opts.format)
+        let decryped_wallet = wallet.decrypt(password.as_bytes());
+        print_result(&wallet, &decryped_wallet, self.seed.as_ref(), opts.format)
     }
 }
 
-pub fn print_result(wallet: &Wallet, result: bool, format: OutputFormat) -> Result {
+pub fn print_result(
+    wallet: &Wallet,
+    decryped_wallet: &Result<Keypair>,
+    seed_type: Option<&SeedType>,
+    format: OutputFormat,
+) -> Result {
     let address = wallet.address().unwrap_or_else(|_| "unknown".to_string());
+    let phrase = decryped_wallet
+        .as_ref()
+        .unwrap()
+        .phrase(seed_type)
+        .join(" ");
+
     match format {
         OutputFormat::Table => {
             let mut table = Table::new();
             table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
-            table.set_titles(row!["Address", "Sharded", "Verify", "PwHash"]);
-            table.add_row(row![address, wallet.is_sharded(), result, wallet.pwhash()]);
+            if seed_type.is_none() {
+                table.set_titles(row!["Address", "Sharded", "Verify", "PwHash"]);
+                table.add_row(row![
+                    address,
+                    wallet.is_sharded(),
+                    decryped_wallet.is_ok(),
+                    wallet.pwhash()
+                ]);
+            } else {
+                table.set_titles(row!["Address", "Sharded", "Verify", "PwHash", "Phrase"]);
+                table.add_row(row![
+                    address,
+                    wallet.is_sharded(),
+                    decryped_wallet.is_ok(),
+                    wallet.pwhash(),
+                    phrase
+                ]);
+            }
             print_table(&table)
         }
         OutputFormat::Json => {
-            let table = json!({
+            let mut table = json!({
                 "address": address,
                 "sharded": wallet.is_sharded(),
-                "verify": result,
+                "verify": decryped_wallet.is_ok(),
                 "pwhash": wallet.pwhash().to_string()
             });
+            if seed_type.is_some() {
+                table["phrase"] = serde_json::Value::String(phrase);
+            }
             print_json(&table)
         }
     }
